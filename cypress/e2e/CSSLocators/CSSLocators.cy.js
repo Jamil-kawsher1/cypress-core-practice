83203015a8ae475c6637d04150de9db37b75f7d0
Jamil kawsher <jamilkawsher@gmail.com>
2023-08-28 12:07:48 +0600

    First Commit

---
+++ cypress/e2e/CSSLocators/CSSLocators.cy.js
@@ -0,0 +1,19 @@
+describe("CSS Locators",()=>{
+
+
+    it("search T Shrit",()=>{
+
+cy.visit("http://www.automationpractice.pl/index.php")
+//Locating the search box and Typing the search Term
+let searchTerm="Printed Chiffon Dress"
+        cy.get("#search_query_top").type(searchTerm)
+        
+        //Clicking On The search icon after finishing the search term typing
+
+        cy.get("[name='submit_search']").click()
+
+        //validating serach Term
+
+        cy.get(".lighter").contains(searchTerm)
+    })
+})
